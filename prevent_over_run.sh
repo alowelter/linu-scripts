91e981d293bb678b54e1744aae8b2f2ab93723bb
Marcelo Welter <60479103+alowelter@users.noreply.github.com>
2021-10-15 15:42:54 -0300

    Criado prevent_over_run.sh

---
+++ prevent_over_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p "$HOME/tmp"
+PIDFILE="$HOME/tmp/myprogram.pid"
+
+if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
+                           grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
+  echo "Already running."
+  exit 99
+fi
+
+/caminho/do/app > $HOME/tmp/myprogram.log &
+
+echo $! > "${PIDFILE}"
+chmod 644 "${PIDFILE}"
